330c7e24b0ff2d8e9cf2d90603d84096604d2345
mitchting <amitchell@ctatax.uk.com>
2024-08-10 10:13:41 +0100

    initial work on About Me app icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./index.css";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -12,6 +16,12 @@ const App = () => {
           <div class="notch">
             <div class="camera"></div>
           </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </div>
       </div>
     </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,10 @@ import "../index.css";
 const Home = () => {
   return (
     <div className="home">
-      <AppIcon
-        icon="/icons/portfolio.png"
-        label="Portfolio"
-        link="/portfolio"
-      />
-      <AppIcon icon="/icons/about.png" label="About Me" link="/about" />
-      <AppIcon icon="/icons/contact.png" label="Contact" link="/contact" />
-      <AppIcon icon="/icons/cv.png" label="CV" link="/cv" />
+      <AppIcon icon="/images/apps/about.png" label="About Me" link="/about" />
+      {/*<AppIcon icon="/icons/portfolio.png" label="Portfolio" link="/portfolio"/>*/}
+      {/*<AppIcon icon="/icons/contact.png" label="Contact" link="/contact" />*/}
+      {/*<AppIcon icon="/icons/cv.png" label="CV" link="/cv" />*/}
     </div>
   );
 };
