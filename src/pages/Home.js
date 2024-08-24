5db5d0382a0ecad315192aad4c7854978d361819
mitchting <amitchell@ctatax.uk.com>
2024-08-24 10:56:17 +0100

    loop over data.appsData array

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,39 @@
 import React from "react";
 import icons from "../components/App";
 import "../index.css";
-import appsData from "../data/data";
+import data from "../data/data";
 
 const Home = () => {
+  const rows = [];
+  let currentRow = [];
+  let currentWidth = 0;
+
+  data.appsData.forEach((app) => {
+    if (currentWidth + app.type > 4) {
+      rows.push(currentRow);
+      currentRow = [];
+      currentWidth = 0;
+    }
+
+    currentRow.push(app);
+    currentWidth += app.type;
+
+    if (currentRow.length > 0) {
+      rows.push(currentRow);
+    }
+  });
+
   return (
     <div className="home">
-      {appsData.map((app) => {
-        return <AppIcon icon={app.icon} label={app.label} link={app.link} />;
+      {data.appsData.map((app) => {
+        return (
+          <icons.app
+            icon={app.icon}
+            label={app.label}
+            link={app.link}
+            type={app.type}
+          />
+        );
       })}
     </div>
   );
