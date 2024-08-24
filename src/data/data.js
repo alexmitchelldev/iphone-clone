dfa27e81b8b7a6b2dc3701f9509598cbb0679174
mitchting <amitchell@ctatax.uk.com>
2024-08-24 10:55:42 +0100

    add appTypes and weather widget, change default export

--- src/data/data.js
+++ src/data/data.js
@@ -1,28 +1,49 @@
+const appTypes = {
+  app: 1,
+  widget: 2,
+};
+
 const appsData = [
   {
     id: 1,
-    label: "About Me",
-    icon: "/images/apps/about.png",
-    link: "/about",
+    label: "Weather",
+    icon: "/images/apps/weather.png",
+    link: null,
+    type: appTypes.widget,
   },
   {
     id: 2,
-    label: "Portfolio",
-    icon: "/images/apps/folder.png",
-    link: "/portfolio",
+    label: "About Me",
+    icon: "/images/apps/about.png",
+    link: "/about",
+    type: appTypes.app,
   },
   {
     id: 3,
-    label: "Skills",
-    icon: "/images/apps/skill.png",
-    link: "/skills",
+    label: "Portfolio",
+    icon: "/images/apps/folder.png",
+    link: "/portfolio",
+    type: appTypes.app,
   },
   {
     id: 4,
+    label: "Skills",
+    icon: "/images/apps/skills.png",
+    link: "/skills",
+    type: appTypes.app,
+  },
+  {
+    id: 5,
     label: "Messages",
     icon: "/images/apps/messages.png",
     link: "/messages",
+    type: appTypes.app,
   },
 ];
 
-export default appsData;
+const data = {
+  appTypes: appTypes,
+  appsData: appsData,
+};
+
+export default data;
